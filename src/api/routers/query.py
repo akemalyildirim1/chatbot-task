1114c72f4baa49251abe90b6612c992858c57cfd
akemalyildirim1 <akemalyildirim1@gmail.com>
2024-06-22 15:02:08 +0300

    ci(workflows/test.yml): fix test pipeline

--- src/api/routers/query.py
+++ src/api/routers/query.py
@@ -2,8 +2,9 @@
 
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from fastapi.responses import ORJSONResponse
+from pydantic import BaseModel, Field
 
 from src.service.query import QueryService
 
@@ -17,19 +18,32 @@ query_router = APIRouter(
 query_service: QueryService = QueryService()
 
 
-@query_router.post(
+class QueryRequest(BaseModel):
+    """Query request class.
+
+    Attributes:
+        query: The query to search.
+    """
+
+    query: str = Field(
+        Query(
+            description="The query to search.",
+        )
+    )
+
+
+@query_router.get(
     "/",
     summary="Query the resources of a user.",
-    # status_code=status.HTTP_200_OK,
 )
 async def query_resources(
-    query: str,
+    query: Annotated[QueryRequest, Depends()],
     session: SessionDep,
     user_teams_id_dependency: Annotated[UserTeamsIdDependency, Depends()],
 ):
     """Query the resources of a user."""
     result = await query_service.query(
-        query=query,
+        query=query.query,
         session=session,
         user_teams_id=user_teams_id_dependency.teams_id,
     )
